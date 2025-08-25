2910b1014a5a464774177b08be4d50a93ccf635f
Supriya9082 <prajapatism24.comp@coeptech.ac.in>
2025-08-25 21:36:11 +0530

    Update ParticipationSection.tsx

--- src/components/ParticipationSection.tsx
+++ src/components/ParticipationSection.tsx
@@ -134,7 +134,7 @@ const ParticipationSection = () => {
           </div>
 
           {/* Registration Process */}
-          < className="bg-gradient-subtle rounded-3xl p-8 md:p-12">
+          < section id="registration" className="bg-gradient-subtle rounded-3xl p-8 md:p-12">
             <div className="grid lg:grid-cols-2 gap-12 items-center">
               {/* Registration Info */}
               <div>
